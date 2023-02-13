525b813b2c498a439ab75184d28d82243f3d68e9
Earsouza <eduardo.ar2001@aluno.ifsc.edu.br>
2023-02-12 23:55:23 -0300

    add]

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.get('/', (req, res) => {
     connection.query('SELECT * FROM EMPRESA', (err, result =>{
         res.send(result);
     }));
-
 });
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT})`));
